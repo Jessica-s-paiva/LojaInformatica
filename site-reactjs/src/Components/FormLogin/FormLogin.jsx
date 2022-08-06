81ea393ce19f46ab7c8f8c08ff4113c09c6753de
Thaynara Cabral <cabralt490@gmail.com>
2022-08-06 11:32:57 -0300

    add validação email

--- site-reactjs/src/Components/FormLogin/FormLogin.jsx
+++ site-reactjs/src/Components/FormLogin/FormLogin.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+
+import React, {useEffect, useState} from 'react'
+
 
 const FormLogin = () => {
+
+  const [Email, setEmail] = useState('')
+  const [Senha, setSenha]= useState('123')
+
+  function handleInputEmail(target){
+    setEmail(target.value)
+  }
+  
+  function handleInputSenha(target){
+    setSenha(target.value)
+  }
+
+  function validaEmail(){
+    if(Email.indexOf('@') > 3 && Email.indexOf('.com') > Email.indexOf('@')){
+      alert("Email Válido")
+    }else{
+      alert("Email Inválido")
+    }
+  }
+
   return (
     <div>
         <form>
         <fieldset>
-          <label htmlFor="">Email:</label>
-          <input type="email"/>
+          <label htmlFor="" >Email:</label>
+          <input type="email" onChange={({target})=>handleInputEmail(target)}/>
         </fieldset>
 
         <fieldset>
@@ -19,7 +41,10 @@ const FormLogin = () => {
           <input type="checkbox"/>
         </fieldset>
 
-        <button>Logar</button>
+        <button onClick={(event)=>{
+          event.preventDefault()
+          validaEmail()
+          }}>Logar</button>
         </form>
 
         <section>
