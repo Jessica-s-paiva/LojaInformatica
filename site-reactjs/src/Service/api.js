800e1f43bcab215a30e70f35b2774deedf4c943b
anafedechem <anafedechem@gmail.com>
2022-08-06 14:20:55 -0300

    Adicionei página de indicação de produtos

--- site-reactjs/src/Service/api.js
+++ site-reactjs/src/Service/api.js
@@ -21,3 +21,27 @@ export const postProduto = async (produto) => {
     const json = await response.data.msg;
     return json;
 }
+
+
+    //mostrar no máximo 3 itens aleatórios na página de indicações
+export const getIndicacoes = async ()=>{
+    const response = await instancia.get('/produtos');
+    const json = await response.data;
+    let tamanho = json.length;
+    if (tamanho <= 3) {
+        return json;
+    }
+
+    function getRandomArbitrary(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    let indicacoes = [];
+    for(let i=0; i<3; i++) {
+        tamanho = json.length;
+        let randomIndex = getRandomArbitrary(0, tamanho-1);
+        let produtoAleatorio = json.splice(randomIndex, 1).pop();
+        indicacoes.push(produtoAleatorio);
+    }
+    return indicacoes;
+}
