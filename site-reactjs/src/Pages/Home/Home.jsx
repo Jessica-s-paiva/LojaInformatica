9ed96487ce07c9fb20307db4fbcff9f0980fbbfd
alebulka <bulkalessandra@outlook.com>
2022-08-08 18:48:40 -0300

    importação da foto na pagina

--- site-reactjs/src/Pages/Home/Home.jsx
+++ site-reactjs/src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import S from './Home.module.css'
+import promocao from '/Users/Ebanx/Desktop/loja-mod5/LojaInformatica/site-reactjs/src/assets/promocao30.png'
 
 const Home = () => {
   return (
     <><div className={S.entrada}>
+          <img className={S.foto} src={promocao} alt="desconto de 30% em produtos selecionados" />
       <p>aqui entra a barra de pesquisa, meio opcional</p>
-      <h1>Foto "carrossel"</h1>
     </div>
     <div className={S.produtos}>
     </div></>
