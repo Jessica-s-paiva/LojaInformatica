dca5f90c01bd0a1be383ebf150d32a677bf452de
alebulka <bulkalessandra@outlook.com>
2022-08-08 17:48:02 -0300

    feat: estrutura pagina home

--- site-reactjs/src/Pages/Home/Home.jsx
+++ site-reactjs/src/Pages/Home/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Cards from '../../Components/Cards/Cards';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <><div className='entrada'>
+      <p>aqui entra a barra de pesquisa, meio opcional</p>
+      <h1>Foto "carrossel"</h1>
+    </div>
+    <div className='produtos'>
+ <p>cards</p>
+    </div></>
   )
 }
 
