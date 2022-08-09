c40204785d67b17e7626bd3c46368692cec9a8b1
alebulka <bulkalessandra@outlook.com>
2022-08-09 11:28:06 -0300

    delete alterações na pagina home

--- site-reactjs/src/Pages/Home/Home.jsx
+++ site-reactjs/src/Pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import S from './Home.module.css'
-import promocao from '/Users/Ebanx/Desktop/loja-mod5/LojaInformatica/site-reactjs/src/assets/sale.png'
+
 
 const Home = () => {
   return (
     <><div className={S.entrada}>
-          <img className={S.foto} src={promocao} alt="desconto de 30% em produtos selecionados" />
-      <p>aqui entra a barra de pesquisa, meio opcional</p>
+
+
     </div>
     <div className={S.produtos}>
     </div></>
