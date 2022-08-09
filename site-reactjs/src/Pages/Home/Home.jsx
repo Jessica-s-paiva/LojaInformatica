2e90f416c87054318dbba0c1e680ed946ce1fc05
Ana-Luiza-Ferreira <analuizaferreira105@gmail.com>
2022-08-09 17:46:26 -0300

    feat:add home

--- site-reactjs/src/Pages/Home/Home.jsx
+++ site-reactjs/src/Pages/Home/Home.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react';
+import Cards from '../../Components/Cards/Cards';
+import {getProdutos} from '../../Service/api.js';
+import pc from '../../assets/pc.jpg'
+import { useParams } from 'react-router-dom'
+
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    handleGetProducts();
+  }, []);
+  async function handleGetProducts() {
+    const response = await getProdutos();
+    setProducts(response);
+}
   return (
-    <div>Home</div>
+    <div>
+      <h1>Loja de Informatica</h1>
+      
+      <section>
+        <picture>
+          <img src={pc} alt="" />
+        </picture>
+      </section>
+      
+      <section>
+          {
+            !!products && products.map((product, index)=>{
+            return (<Cards produto={product} key={index}/>)
+            })
+          }
+        </section>
+    </div>
+
   )
 }
 
